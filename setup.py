03ca0cd7615327911b4cc2eda789c2ab88acd082
Dario Fadda <darioflute@gmail.com>
2021-07-29 21:58:44 -0700

    added setup files

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+"""Setup script fo installing the fififly library."""
+
+from distutils.core import setup
+
+config = {
+    'name': 'fififly',
+    'version': '0.1',
+    'description': 'FIFI-LS flight Python library',
+    'long_description': 'Collection of programs to prepare FIFI-LS data',
+    'author': 'Dario Fadda',
+    'author_email': 'darioflute@gmail.com',
+    'url': 'https://github.com/darioflute/fififly.git',
+    'download_url': 'https://github.com/darioflute/fififly',
+    'license': 'GPLv3+',
+    'packages': ['fififly'],
+    'include_package_data': True,
+    'package_data': {'fifipy': ['test/*.aor','tes/*.misxml']},
+    'install_requires': ['numpy', 'lxml', 'matplotlib', 'astropy']
+}
+
+setup(**config)
