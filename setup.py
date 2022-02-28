42d8d64813673380544bd1933094d2b7fe2e3e7a
Dario Fadda <darioflute@gmail.com>
2022-02-27 22:43:56 -0800

    try 31

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
-"""Setup script fo installing the fifipy library."""
+"""Setup script fo installing the fififly library."""
 
 from distutils.core import setup
 
 config = {
     'name': 'fififly',
-    'version': '1.0.30',
+    'version': '1.0.31',
     'description': 'FIFI-LS Python library',
     'long_description': 'Collection of programs to reduce FIFI-LS data',
     'author': 'Dario Fadda',
@@ -16,11 +16,7 @@ config = {
     'packages': ['fififly','fififly.scanmaker'],
     'scripts': ['bin/scanmaker'],
     'include_package_data': True,
-    'package_data': {'fififly': ['data/*png','version.json',
-                                 'scanmaker/data/*txt',
-                                 'scanmaker/greenstylesheet.css',
-                                 'scanmaker/icons/*png',
-                                 'scanmaker/copyright.txt']}
+    'package_data': {'fififly': 'fififly': ['data/*png','version.json','scanmaker/data/*txt','scanmaker/greenstylesheet.css','scanmaker/icons/*png','scanmaker/copyright.txt']},
     'install_requires': ['numpy', 'matplotlib', 'astropy']
 }
 
