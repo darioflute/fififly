cf22201c324f8c2cabf08cd39458da84126ab20c
Dario Fadda <darioflute@gmail.com>
2022-02-27 20:06:23 -0800

    new try

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ config = {
     'scripts': ['bin/scanmaker'],
     'include_package_data': True,
     'package_data': {'fififly': ['data/*png','scanmaker/data/*txt','version.json','scanmaker/greenstylesheet.css','scanmaker/icons/*png','scanmaker/copyright.txt']},
-    'install_requires': ['numpy', 'matplotlib', 'astropy','lxml','unidecode','pyqt']
+    'install_requires': ['numpy', 'matplotlib', 'astropy','lxml','unidecode']
 }
 
 setup(**config)
